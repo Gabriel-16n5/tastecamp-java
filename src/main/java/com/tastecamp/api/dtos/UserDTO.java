29dd70c7dab0b40934be56795bf157afc74581c4
GabrielIGNS <gabrielgranjeia@alunos.utfpr.edu.br>
2024-02-01 15:46:56 -0300

    feat: created user model and tdo

---
+++ src/main/java/com/tastecamp/api/dtos/UserDTO.java
@@ -0,0 +1,17 @@
+package com.tastecamp.api.dtos;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class UserDTO {
+
+    @NotBlank
+    private String username;
+
+    @NotBlank
+    @Email
+    private String email;
+    
+}
---
+++ src/main/java/com/tastecamp/api/models/UserModel.java
@@ -0,0 +1,29 @@
+package com.tastecamp.api.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class UserModel {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+}

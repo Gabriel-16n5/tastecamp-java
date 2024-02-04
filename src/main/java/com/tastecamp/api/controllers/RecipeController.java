a7c38af6efcb9952a70c05dacd3b9d250b1c37da
GabrielIGNS <gabrielgranjeia@alunos.utfpr.edu.br>
2024-02-04 15:44:44 -0300

    update RecipeController.java, RecipeService.java and application.properties

--- src/main/java/com/tastecamp/api/controllers/RecipeController.java
+++ src/main/java/com/tastecamp/api/controllers/RecipeController.java
@@ -11,7 +11,6 @@ import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
-import java.util.Optional;
 
 
 import org.springframework.http.HttpStatus;
--- src/main/java/com/tastecamp/api/services/RecipeService.java
+++ src/main/java/com/tastecamp/api/services/RecipeService.java
@@ -1,7 +1,6 @@
 package com.tastecamp.api.services;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
